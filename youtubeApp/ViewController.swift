a4f865715b77b46f0d5832bbef78c9639ef49e8b
Dawid Jaskulski <dawid@MacBook-Pro-Dawid.local>
2020-06-25 17:57:21 +0200

    initial xcode project

---
+++ youtubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtubeApp
+//
+//  Created by Dawid Jaskulski on 25/06/2020.
+//  Copyright © 2020 Dawid Jaskulski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
